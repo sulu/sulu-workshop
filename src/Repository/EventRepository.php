9a32a4077c03f752d4c79497ca8d4ab2fee41420
Johannes Wachter <johannes@sulu.io>
2019-09-19 11:07:17 +0000

    Add content-type, data-provider and event page

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -7,6 +7,10 @@ namespace App\Repository;
 use App\Entity\Event;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\Query\Expr\Join;
+use Doctrine\ORM\QueryBuilder;
+use Sulu\Component\SmartContent\Orm\DataProviderRepositoryInterface;
+use Sulu\Component\SmartContent\Orm\DataProviderRepositoryTrait;
 
 /**
  * @method Event|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,8 +18,12 @@ use Doctrine\Common\Persistence\ManagerRegistry;
  * @method Event[]    findAll()
  * @method Event[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class EventRepository extends ServiceEntityRepository
+class EventRepository extends ServiceEntityRepository implements DataProviderRepositoryInterface
 {
+    use DataProviderRepositoryTrait {
+        findByFilters as parentFindByFilters;
+    }
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Event::class);
@@ -52,6 +60,26 @@ class EventRepository extends ServiceEntityRepository
         $event->setLocale($locale);
 
         return $event;
+    }
+
+    public function findByFilters($filters, $page, $pageSize, $limit, $locale, $options = [])
+    {
+        $entities = $this->parentFindByFilters($filters, $page, $pageSize, $limit, $locale, $options);
+
+        return array_map(
+            function (Event $entity) use ($locale) {
+                return $entity->setLocale($locale);
+            },
+            $entities
+        );
+    }
+
+    protected function appendJoins(QueryBuilder $queryBuilder, string $alias, string $locale): void
+    {
+        $queryBuilder->innerJoin($alias . '.translations', 'translation', Join::WITH, 'translation.locale = :locale');
+        $queryBuilder->setParameter('locale', $locale);
+
+        $queryBuilder->andWhere($alias . '.enabled = true');
     }
 
     // /**
---
+++ tests/Functional/Traits/PageTrait.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Traits;
+
+use Sulu\Bundle\PageBundle\Document\PageDocument;
+use Sulu\Component\Content\Document\WorkflowStage;
+use Sulu\Component\DocumentManager\DocumentManagerInterface;
+
+trait PageTrait
+{
+    /**
+     * @param mixed[] $data
+     */
+    protected function createPage(
+        string $template,
+        string $webspaceKey,
+        array $data,
+        string $locale = 'en'
+    ): PageDocument {
+        $documentManager = $this->getDocumentManager();
+
+        /** @var PageDocument $document */
+        $document = $documentManager->create('page');
+
+        if (!$document instanceof PageDocument) {
+            throw new \RuntimeException('Invalid document');
+        }
+
+        $document->setLocale($locale);
+        $document->setTitle($data['title']);
+        $document->setStructureType($template);
+        $document->setResourceSegment($data['url']);
+
+        if ($data['published']) {
+            $document->setWorkflowStage(WorkflowStage::PUBLISHED);
+        }
+
+        $document->getStructure()->bind($data);
+
+        $documentManager->persist($document, $locale, ['parent_path' => '/cmf/' . $webspaceKey . '/contents']);
+
+        if ($data['published']) {
+            $documentManager->publish($document, $locale);
+        }
+
+        $documentManager->flush();
+
+        return $document;
+    }
+
+    abstract protected function getDocumentManager(): DocumentManagerInterface;
+}
