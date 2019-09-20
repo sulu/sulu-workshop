b80ac1e5d93cd9f41496862782a1527093a82f73
Johannes Wachter <johannes@sulu.io>
2019-09-20 08:47:04 +0000

    Use abstract-controller as base for event-website-controller

--- src/Controller/EventWebsiteController.php
+++ src/Controller/EventWebsiteController.php
@@ -6,50 +6,39 @@ namespace App\Controller;
 
 use App\Repository\EventRepository;
 use Sulu\Bundle\WebsiteBundle\Resolver\TemplateAttributeResolverInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Twig\Environment;
 
-class EventWebsiteController
+class EventWebsiteController extends AbstractController
 {
-    /**
-     * @var EventRepository
-     */
-    private $eventRepository;
-
-    /**
-     * @var Environment
-     */
-    private $twig;
-
-    /**
-     * @var TemplateAttributeResolverInterface
-     */
-    private $attributesResolver;
-
-    public function __construct(
-        EventRepository $eventRepository,
-        Environment $twig,
-        TemplateAttributeResolverInterface $attributesResolver
-    ) {
-        $this->eventRepository = $eventRepository;
-        $this->twig = $twig;
-        $this->attributesResolver = $attributesResolver;
-    }
-
     public function indexAction(int $id, Request $request): Response
     {
-        $event = $this->eventRepository->findById($id, $request->getLocale());
+        $event = $this->get(EventRepository::class)->findById($id, $request->getLocale());
         if (!$event) {
             throw new NotFoundHttpException();
         }
 
-        $content = $this->twig->render(
+        return $this->render(
             'events/index.html.twig',
-            $this->attributesResolver->resolve(['event' => $event, 'content' => ['title' => $event->getTitle()]])
+            $this->get(TemplateAttributeResolverInterface::class)->resolve(
+                [
+                    'event' => $event,
+                    'content' => ['title' => $event->getTitle()],
+                ]
+            )
         );
+    }
 
-        return new Response($content);
+    public static function getSubscribedServices(): array
+    {
+        return array_merge(
+            parent::getSubscribedServices(),
+            [
+                EventRepository::class,
+                TemplateAttributeResolverInterface::class,
+            ]
+        );
     }
 }
