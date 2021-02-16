e846e8c34384f9cd0b759da06db1463d8383e7b5
nnatter <niklas@sulu.io>
2021-02-16 15:22:36 +0100

    Improve rebase-assignment-branches script to pick only last commit (#32)

--- bin/rebase-assignment-branches.sh
+++ bin/rebase-assignment-branches.sh
@@ -5,51 +5,51 @@ git fetch origin
 
 git checkout assignment/01
 git reset --hard origin/assignment/01
-git rebase origin/master
+git rebase --onto origin/master HEAD^1
 
 git checkout assignment/02
 git reset --hard origin/assignment/02
-git rebase assignment/01
+git rebase --onto assignment/01 HEAD^1
 
 git checkout assignment/03
 git reset --hard origin/assignment/03
-git rebase assignment/02
+git rebase --onto assignment/02 HEAD^1
 
 git checkout assignment/04
 git reset --hard origin/assignment/04
-git rebase assignment/03
+git rebase --onto assignment/03 HEAD^1
 
 git checkout assignment/05
 git reset --hard origin/assignment/05
-git rebase assignment/04
+git rebase --onto assignment/04 HEAD^1
 
 git checkout assignment/06
 git reset --hard origin/assignment/06
-git rebase assignment/05
+git rebase --onto assignment/05 HEAD^1
 
 git checkout assignment/07
 git reset --hard origin/assignment/07
-git rebase assignment/06
+git rebase --onto assignment/06 HEAD^1
 
 git checkout assignment/08
 git reset --hard origin/assignment/08
-git rebase assignment/07
+git rebase --onto assignment/07 HEAD^1
 
 git checkout assignment/09
 git reset --hard origin/assignment/09
-git rebase assignment/08
+git rebase --onto assignment/08 HEAD^1
 
 git checkout assignment/10
 git reset --hard origin/assignment/10
-git rebase assignment/09
+git rebase --onto assignment/09 HEAD^1
 
 git checkout assignment/11
 git reset --hard origin/assignment/11
-git rebase assignment/10
+git rebase --onto assignment/10 HEAD^1
 
 git checkout assignment/12
 git reset --hard origin/assignment/12
-git rebase assignment/11
+git rebase --onto assignment/11 HEAD^1
 
 echo
 git --no-pager log --oneline -n 13
